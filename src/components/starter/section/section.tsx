da75f78550d75251a5f4a8acd9eb6686693efd32
Linh Nguyen <linh.nguyenkhanhib@gmail.com>
2023-05-09 23:22:46 +0300

    refactor folders structure

--- src/components/starter/section/section.tsx
+++ src/components/starter/section/section.tsx
@@ -1,42 +1,11 @@
 import { component$ } from "@builder.io/qwik";
-
+import ImagePanel from "../images/image-panel";
+import Info from "../info/info";
 export default component$(() => {
   return (
     <section class="flex mt-10">
-      <div class="flex-1">red</div>
-      <div class="flex-1">
-        <h1 class="uppercase font-semibold text-orange">sneaker company</h1>
-        <h1 class="font-bold text-4xl tracking-wide leading-normal">
-          Fall Limited{" "}
-        </h1>
-        <h1 class="font-bold text-4xl tracking-wide leading-10">Sneakers</h1>
-        <div class="break-all text-grayishblue leading-relaxed tracking-wide mt-4">
-          {" "}
-          <p class="">
-            These low-profile sneakers are your perfect casual wear companion.
-            Featuring a durable rubber outer sole, they’ll withstand everything
-            the weather can offer.
-          </p>
-        </div>
-        <div class="mt-4 flex items-center text-center space-x-6">
-          <h3 class="font-bold text-2xl">$125.00</h3>{" "}
-          <h3 class="text-md font-semibold text-orange bg-paleorange w-15 text-center rounded-lg px-2 py-0.5">
-            50%{" "}
-          </h3>
-        </div>
-        <h5 class="text-lg text-grayishblue line-through mt-2">$250.00</h5>
-
-        <div>
-          <button>-</button>
-          <input type="number" />
-          <button>+</button>
-          <button>
-            {" "}
-            <img src="images/icon-cart.svg" alt="cart" class="" />
-            Add to cart
-          </button>
-        </div>
-      </div>
+      <ImagePanel />
+      <Info />
     </section>
   );
 });
