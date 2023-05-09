cb8a36daa5c856f7723e40c5ad1b98772da72ca4
Linh Nguyen <linh.nguyenkhanhib@gmail.com>
2023-05-09 22:34:45 +0300

    style navbar

--- src/components/starter/header/header.tsx
+++ src/components/starter/header/header.tsx
@@ -1,16 +1,27 @@
 import { component$ } from "@builder.io/qwik";
-
+import { Link } from "@builder.io/qwik-city";
 export default component$(() => {
   return (
-    <nav>
-      <img>logo</img>
-      <ul>
-        <li>Collections</li>
-        <li>Men</li>
-        <li>Women</li>
-        <li>About</li>
-        <li>Contact</li>
-      </ul>
-    </nav>
+    <>
+      <nav class="flex items-center justify-start w-full h-15 p-7 space-x-10">
+        <img src="images/logo.svg" alt="logo" width={150} />
+        <ul class="flex space-x-20 text-darkgrayishblue cursor-pointer">
+          <Link class="hover:text-verydarkblue">Collections</Link>
+          <Link class="hover:text-verydarkblue">Men</Link>
+          <Link class="hover:text-verydarkblue">Women</Link>
+          <Link class="hover:text-verydarkblue">About</Link>
+          <Link class="hover:text-verydarkblue">Contact</Link>
+        </ul>{" "}
+        <div class="flex justify-end items-center space-x-10 ml-2 cursor-pointer ">
+          <div class="ml-60">
+            <img src="images/icon-cart.svg" alt="cart" class="" />
+          </div>
+          <div class="border-4 rounded-full hover:border-orange">
+            <img src="images/image-avatar.png" alt="avatar" width={60} />
+          </div>
+        </div>
+      </nav>
+      <div class="m-auto bg-lightgrayishblue w-[90%] h-1 "></div>
+    </>
   );
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        orange: "hsl(26, 100%, 55%)",
+        paleorange: "hsl(25, 100%, 94%)",
+        verydarkblue: "hsl(220, 13%, 13%)",
+        darkgrayishblue: "hsl(219, 9%, 45%)",
+        grayishblue: "hsl(220, 14%, 75%)",
+        lightgrayishblue: "hsl(223, 64%, 98%)",
+        white: "#ffffff",
+      },
+    },
   },
   plugins: [],
 };
